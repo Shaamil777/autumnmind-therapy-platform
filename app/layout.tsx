e680a5d830b08a547d58a36c7ac56a8c724f1e17
Shaamil777 <shamilm2324@gmail.com>
2026-06-10 21:17:59 +0530

    feat:code cleanup , removed unwanted files and all

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,13 @@ import "./globals.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import FloatingToast from "./components/ui/FloatingToast";
-import { Quicksand, Playfair_Display, Inter, Cormorant_Garamond } from 'next/font/google';
+import { Quicksand, Cormorant_Garamond } from 'next/font/google';
 import type { Metadata } from "next";
 
 const quicksand = Quicksand({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-quicksand',
-});
-
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  weight: ['700'],
-  variable: '--font-playfair',
-});
-
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['400'],
-  variable: '--font-inter',
 });
 
 const cormorantGaramond = Cormorant_Garamond({
@@ -41,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${quicksand.variable} ${playfair.variable} ${inter.variable} ${cormorantGaramond.variable} font-quicksand`}>
+      <body className={`${quicksand.variable} ${cormorantGaramond.variable} font-quicksand`}>
         <Navbar />
         {children}
         <Footer />
